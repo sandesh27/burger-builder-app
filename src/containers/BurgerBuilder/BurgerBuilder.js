447cfbd4604dbe5fb23eb96b447fa7881cd7093e
sandesh magdum <sandesh.s_magdum@yahoo.co.in>
2019-07-05 18:39:19 +0530

    Added burger builder components

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxilliary";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>
+          <Burger />
+        </div>
+        <div>Burger Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
